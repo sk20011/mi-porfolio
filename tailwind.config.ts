c55c3029974d405f6f8a606c3c3bf603c5ed30bd
Solomon <solomonkasparov@gmai.com>
2024-07-14 21:37:01 +0200

    guradado version 2.0

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        secondary:"#f5741c",
-        darkBg: "#131424"
+        secondary: "#f5741c",
+        darkBg: "#131424",
       },
       backgroundImage: {
         "gradient-cover":
-        "linear-gradient(90, 21deg, rgba(70, 54, 124, 0.5) -5.91% rgba(74, 47, 189, 0.5)111.58%"
+          "linear-gradient(90.21deg, rgba(170, 54, 124, 0.5) -5.91%, rgba(74, 47, 189, 0.5) 111.58%)",
       },
     },
   },
